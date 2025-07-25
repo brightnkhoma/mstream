8fea5874d8fa523a6a6d49de0223e8cc453909d4
bright nkhoma <gis-020-20@must.ac.mw>
2025-07-26 00:02:22 +0200

    update

--- api/index.ts
+++ api/index.ts
@@ -3,26 +3,24 @@ import cors from 'cors';
 import router from './lib/routes/route';
 import serverlessExpress from '@vendia/serverless-express';
 
-
 const app = express();
-const PORT = 4000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*", 
     credentials: true,
   })
 );
-app.use(express.raw({ type: 'application/json' }));
 app.use(express.json());
-
 app.use(router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express + TypeScript!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-export default app
+// app.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+
+// ✅ Instead, export the handler
+export const handler = serverlessExpress({ app });
