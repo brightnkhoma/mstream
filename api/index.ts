4ce24567d5c0a0cc537e42f409e2286762168c89
bright nkhoma <gis-020-20@must.ac.mw>
2025-07-25 23:33:51 +0200

    update config json vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import router from './lib/routes/route';
+import serverlessExpress from '@vendia/serverless-express';
 
 
 const app = express();
@@ -19,3 +20,4 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+export default serverlessExpress({ app });
