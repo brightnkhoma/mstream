0af504cf0f5b1cb409bef13218764555baebc9fb
bright nkhoma <gis-020-20@must.ac.mw>
2025-07-26 00:11:35 +0200

    change2

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,11 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import router from './lib/routes/route';
-import serverlessExpress from '@vendia/serverless-express';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*", 
-    credentials: true,
-  })
-);
-
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(router);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express + TypeScript!');
-});
-
-
-export default serverlessExpress({ app });
+module.exports = app;
