8212b6bac8a529dd416202d83656db8e37105ccb
bright nkhoma <gis-020-20@must.ac.mw>
2025-07-26 00:19:28 +0200

    options

--- api/index.ts
+++ api/index.ts
@@ -5,6 +5,10 @@ import router from './lib/routes/route';
 const app = express();
 
 app.use(cors({ origin: '*', credentials: true }));
+app.options('*', cors()); 
+router.options('*', cors()); 
+
+
 app.use(express.json());
 app.use(router);
 
